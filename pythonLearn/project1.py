a81a3d2af78d6f5fe5d5e18e6340080d6c7e8ec5
rofthchild <130942967+rofthchild@users.noreply.github.com>
2025-07-17 15:59:51 -0500

    fix bugs, confirm in terminal

--- pythonLearn/project1.py
+++ pythonLearn/project1.py
@@ -9,17 +9,17 @@
 import random
 ## import the random module which can be used to generate random numbers
 
-questions {
+questions = {
     "What is the keyword to define a function in Python?": "def",
     "Which data type is used to store True or False values?": "boolean",
     "What is the correct file extension for Python files?": "•py",
-    "Which symbol is used to comment in Python?": "#"
+    "Which symbol is used to comment in Python?": "#",
     "What function is used to get input from the user?": "input",
     "How do you start a for loop in Python?": "for",
     "What is the output of 2 ** 3 in Python?": "8",
     "What keyword is used to import a module in Python?": "import",
     "What does the len() function return?": "length",
-    "What is the result of 10 // 3 in Python?": "3""
+    "What is the result of 10 // 3 in Python?": "3"
 }
 
 ## created a dictionary which is just a Key and Value pair
